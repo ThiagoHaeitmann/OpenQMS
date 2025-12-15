f34f2534ab7b60b10508dffa3d04ebda48296bce
Thiago Haeitmann <135155756+ThiagoHaeitmann@users.noreply.github.com>
2025-12-15 10:24:13 -0300

    Switch database context from SQLite to SQL Server

--- Program.cs
+++ Program.cs
@@ -31,8 +31,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    //options.UseSqlServer(connectionString));
-    options.UseSqlite(connectionString));
+    options.UseSqlServer(connectionString));
+    //options.UseSqlite(connectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
